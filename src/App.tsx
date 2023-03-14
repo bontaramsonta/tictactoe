16e6eefcf96570318325ce4fb1480b9a41204203
bontaramsonta <18cs113@gmail.com>
2023-03-14 23:44:51 +0530

    optimize: refactoring functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,6 @@
 import React from "react";
 
-const getChar = (turn: number) => (turn === 1 ? "X" : "O");
-
-const getReadablePosition = (index: number) => {
-  const result: string[] = [];
-  if (index === 4) result.push("center");
-  if ([1, 3, 5, 7].includes(index)) result.push("middle");
-  if ([0, 1, 2].includes(index)) result.push("top");
-  if ([6, 7, 8].includes(index)) result.push("bottom");
-  if ([0, 3, 6].includes(index)) result.push("left");
-  if ([2, 5, 8].includes(index)) result.push("right");
-  return result.join(" ");
-};
+const setRandomTurn = () => Math.floor(Math.random() * 2 + 1);
 
 const Score: React.FC<{ score: [number, number] }> = ({ score }) => {
   return (
@@ -22,51 +11,48 @@ const Score: React.FC<{ score: [number, number] }> = ({ score }) => {
 };
 
 function App() {
-  const setRandomTurn = () => Math.floor(Math.random() * 2 + 1);
-
-  const [gameState, setGameState] = React.useState(new Array(9).fill(null));
+  const [gameState, setGameState] = React.useState<Array<string | null>>(
+    new Array(9).fill(null)
+  );
   const [turn, setTurn] = React.useState(setRandomTurn()); // 1,2
   const [result, setResult] = React.useState<string | null>(null);
   const [score, setScore] = React.useState<[number, number]>([0, 0]);
   const [moves, setMoves] = React.useState<[number, number][]>([]); // [index, turn]
 
-  const checkIfWon = () => {
-    for (const turn of [1, 2]) {
-      const char = getChar(turn);
-      // column check
-      for (let i = 0; i < 3; i++) {
-        const [first, second, third] = [
-          gameState[i],
-          gameState[i + 3],
-          gameState[i + 6],
-        ];
-        if (first === char && first === second && second === third) {
-          return turn;
-        }
-      }
-      // row check
-      for (let i = 0; i < 3; i++) {
-        const [first, second, third] = [
-          gameState[i * 3],
-          gameState[i * 3 + 1],
-          gameState[i * 3 + 2],
-        ];
-        if (first === char && first === second && second === third) {
-          return turn;
-        }
-      }
-      // diagonal check
-      if (
-        [gameState[0], gameState[4], gameState[8]].every(
-          (cell) => cell === char
-        ) ||
-        [gameState[2], gameState[4], gameState[6]].every(
-          (cell) => cell === char
-        )
-      ) {
-        return turn;
-      }
+  const char = React.useMemo(() => (turn === 1 ? "X" : "O"), [turn]);
+  const getReadablePosition = React.useCallback(
+    (index: number) => {
+      const result: string[] = [];
+      if (index === 4) result.push("center");
+      if ([1, 3, 5, 7].includes(index)) result.push("middle");
+      if ([0, 1, 2].includes(index)) result.push("top");
+      if ([6, 7, 8].includes(index)) result.push("bottom");
+      if ([0, 3, 6].includes(index)) result.push("left");
+      if ([2, 5, 8].includes(index)) result.push("right");
+      return result.join(" ");
+    },
+    [moves]
+  );
+
+  const checkIfWon = (gameState: Array<string | null>) => {
+    const lines = [
+      // rows
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      //columns
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 7],
+      //diagonals
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (const line of lines) {
+      const [first, second, third] = line.map((index) => gameState[index]);
+      if (first !== null && first === second && second === third) return first;
     }
+    return null;
   };
 
   const play = (index: number) => {
@@ -74,12 +60,13 @@ function App() {
     if (result) return;
     // selected box is already filled
     if (gameState[index] !== null) return;
-    console.log("[played]", turn, index, gameState[index]);
-    const char = getChar(turn);
-    const newGameState = gameState;
+
+    // set new game state
+    const newGameState = [...gameState];
     newGameState.splice(index, 1, char);
     setGameState(newGameState);
-    const won = checkIfWon();
+    const won = checkIfWon(newGameState);
+    // set score and result if somebody won
     if (won) {
       setScore(
         score.map((score, i) => (i + 1 === turn ? score + 1 : score)) as [
@@ -89,17 +76,17 @@ function App() {
       );
       setResult(`Player ${turn} won`);
     }
-    // all boxes are filled
+    // all boxes are filled and nobody won
     if (newGameState.every((move) => move !== null)) {
       setResult(`Nobody won`);
     }
+    // append the new move to the moves list
     setMoves([...moves, [index, turn]]);
     setTurn(turn === 1 ? 2 : 1);
   };
 
   const undo = (index: number) => {
     if (result) return;
-    console.log("[undo]", index);
     const movesToUndo = moves.slice(index);
     const newGameState = [...gameState];
     movesToUndo.forEach((move) => (newGameState[move[0]] = null));
@@ -123,7 +110,6 @@ function App() {
           <p>{result}</p>
           <button
             onClick={() => {
-              console.log("[start new game]");
               setResult(null);
               setMoves([]);
               setGameState(new Array(9).fill(null));
@@ -141,7 +127,7 @@ function App() {
           </button>
         </div>
       ) : (
-        <p>{`its Player ${turn}'s turn (${getChar(turn)})`}</p>
+        <p>{`its Player ${turn}'s turn (${char})`}</p>
       )}
       {moves.length > 0 && (
         <ul>
@@ -150,9 +136,7 @@ function App() {
             <li key={index}>
               <button onClick={undo.bind(null, index)}>{`Player ${
                 move[1]
-              } put ${getChar(move[1])} at ${getReadablePosition(
-                move[0]
-              )}`}</button>
+              } put ${char} at ${getReadablePosition(move[0])}`}</button>
             </li>
           ))}
         </ul>
